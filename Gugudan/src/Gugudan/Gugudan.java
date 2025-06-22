0054bac1b107b5b78363cd6e5b169e86febecca8
HwangEunHye <HwangEunHye@kb02374270>
2025-06-22 19:31:34 +0900

    25/06/22 gugudan

--- Gugudan/src/Gugudan/Gugudan.java
+++ Gugudan/src/Gugudan/Gugudan.java
@@ -1,7 +1,72 @@
 package Gugudan;
 
+import java.util.Scanner;
+
+// Run : ctrl + F11
+// 라인 복사 : ctrl + alt + ⇅
+// 구간 주석 : 드래그 후 ctal + //
+
 public class Gugudan {
 	public static void main(String[] args) {
-		System.out.println();
+//		System.out.println("2단");
+//		System.out.println(2 * 1);
+//		System.out.println(2 * 2);
+//		System.out.println(2 * 3);
+//		System.out.println(2 * 4);
+//		System.out.println(2 * 5);
+//		System.out.println(2 * 6);
+//		System.out.println(2 * 7);
+//		System.out.println(2 * 8);
+//		System.out.println(2 * 9);
+//		System.out.println("---------------");
+//		
+//		System.out.println("3단");
+//		System.out.println(3 * 1);
+//		System.out.println(3 * 2);
+//		System.out.println(3 * 3);
+//		System.out.println(3 * 4);
+//		System.out.println(3 * 5);
+//		System.out.println(3 * 6);
+//		System.out.println(3 * 7);
+//		System.out.println(3 * 8);
+//		System.out.println(3 * 9);
+//		System.out.println("---------------");
+		
+//		System.out.println("4단");
+//		int result = 4 * 1;//변수활용 main의 지역변수 중괄호 밖에선 사라짐 stack메모리에 저장
+//		System.out.println(result);
+//		result = 4 * 2;
+//		System.out.println(result);
+//		result = 4 * 3;
+//		System.out.println(result);
+//		result = 4 * 4;
+//		System.out.println(result);
+//		result = 4 * 5;
+//		System.out.println(result);
+//		result = 4 * 6;
+//		System.out.println(result);
+//		result = 4 * 7;
+//		System.out.println(result);
+//		result = 4 * 8;
+//		System.out.println(result);
+//		result = 4 * 9;
+//		System.out.println(result);
+//		System.out.println("---------------");
+		
+//		System.out.println("구구단을 출력할 단은?");
+//		Scanner sc = new Scanner(System.in);
+//		int number = sc.nextInt();
+//		System.out.println(number * 1);
+//		System.out.println(number * 2);
+//		System.out.println(number * 3);
+//		System.out.println(number * 4);
+//		System.out.println(number * 5);
+//		System.out.println(number * 6);
+//		System.out.println(number * 7);
+//		System.out.println(number * 8);
+//		System.out.println(number * 9);
+//		System.out.println("---------------");
+		
+		
 	}
 }
