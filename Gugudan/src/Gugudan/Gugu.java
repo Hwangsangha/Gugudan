0aa80a019181c33b62a4ebc1a45ca09d4f94f7a1
User <User@192.168.123.101>
2025-06-24 21:01:15 +0900

    25/06/24 gugu

---
+++ Gugudan/src/Gugudan/Gugu.java
@@ -0,0 +1,26 @@
+package Gugudan;
+
+import java.util.Scanner;
+
+public class Gugu {
+	static Scanner sc = new Scanner(System.in);
+	static String inputValue = sc.nextLine();
+	
+	static String[] splitedValue = inputValue.split(",");
+	
+	static int first = Integer.parseInt(splitedValue[0]);
+	static int second = Integer.parseInt(splitedValue[1]);
+	
+	public static void main(String[] args) {
+		System.out.println("입력값은?");
+		guguCalc(inputValue);
+	}
+	
+	public static void guguCalc(String inputValue) {
+		for (int i = 2; i <= first; i++) {
+			for (int j = 1; j <= second; j++) {
+				System.out.println(i + "*" + j + "=" + i * j);
+			}
+		}
+	}
+}
